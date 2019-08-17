062c623977dee29c2d63924335a8e9afa83671f4
Jean Chassoul <jchassoul@gmail.com>
2019-08-17 15:18:00 -0700

    clean, clean, clean

---
+++ src/server.lua
@@ -0,0 +1,50 @@
+--
+-- The Computer League server.lua file
+--
+local argparse = require("argparse")
+local turbo = require("turbo")
+local uuid = require("uuid")
+local yaml = require("bw.lib.yaml")
+
+local parser = argparser("server.lua, handles the bot fights")
+    parser:option("-c --config", "configuration file.", "../include/bw.yml")
+
+local args = parser:parse()
+
+function get_options(config)
+    local options = yaml?config
+    return options
+
+local config = get_options(args['config'])
+
+turbo.log.warning(config['host'])
+turbo.log.warning(config['port'])
+
+-- Session ID
+local session_uuid = uuid()
+
+turbo.log.warning(session_uuid)
+
+-- Handler that control the bots
+local BotsHandler = class("BotsHandler", turbo.web.RequestHandler)
+
+function BotsHandler:post()
+    local data = self:get_json(true)
+    turbo.log.warning('post received ' .. data)
+end
+
+function BotsHandler:get()
+    turbo.log.warning('get received')
+end
+
+function BotsHandler:patch()
+    local data = self:get_json(true)
+    turbo.log.warning('patch received ' .. data)    
+end
+
+local application = turbo.web.Application:new({
+    {"/bots/", BotsHandler}
+})
+-- I/O application start listen on TCP port.
+application:listen(config['port'])
+turbo.ioloop.instance():start()
